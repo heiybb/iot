87c6563f806b84650abea6e23b28a428e3ea91a5
heiybb <hf.heiybb@gmail.com>
2019-03-23 16:39:02 +1100

    add temperature and humidity precision limit

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -1,36 +1,45 @@
 import datetime
-import json
+import PushCheck
 import SQLiteLib
-import virtual_sense_hat
+
 from virtual_sense_hat import VirtualSenseHat
+from pushbullet import Pushbullet
+
+pb = Pushbullet('o.HyVkzj8lKJWZ4t0i8fzGijVDwnkDKonS')
 
 
 class DataJson:
     def __init__(self, timeslot, temperature, humidity):
         self.timeslot = timeslot
-        self.temperature = temperature
-        self.humidity = humidity
+        self.temperature = float(format(temperature, '.2f'))
+        self.humidity = float(format(humidity, '.2f'))
+
+    def to_string(self):
+        return 'Time:' + str(self.timeslot) + '\n' + \
+               'Temperature: {0:0.2f} ℃'.format(self.temperature) + '\n' + \
+               'Humidity: {0:0.2f} %'.format(self.humidity)
 
 
 def get_temp_humidity():
     sense = VirtualSenseHat.getSenseHat()
     sense.clear()
     timeslot = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-
     temperature = sense.get_temperature()
     # sense.show_message('Temperature: {0:0.2f} ℃'.format(temperature))
-
     humidity = sense.get_humidity()
     # sense.show_message('Humidity: {0:0.2f} %'.format(humidity))
-    SQLiteLib.insert_data(timeslot, temperature, humidity)
+    newSege = DataJson(timeslot, temperature, humidity)
+    SQLiteLib.insert_data(newSege.timeslot, newSege.temperature, newSege.humidity)
+    try_push(newSege)
 
 
-def compare():
-    with open('config.json', 'r', encoding='utf-8') as conf:
-        conf = json.load(conf)
+def try_push(segemant: DataJson):
+    today_utc = datetime.datetime.utcnow().strftime('%Y-%m-%d')
+    if not PushCheck.query_send(today_utc):
+        pb.push_note("Raspberry Notify", segemant.to_string())
 
 
 if __name__ == '__main__':
     SQLiteLib.initialize()
     get_temp_humidity()
-    SQLiteLib.query_all()
+    SQLiteLib.query_all_th_data()
