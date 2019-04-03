8960a32142c5db559da723e97e626073c56c9aa3
Hubert Law <s3706523@student.rmit.edu.au>
2019-04-03 11:10:26 +0800

    redesign with OO

--- analytics.py
+++ analytics.py
@@ -1,31 +1,50 @@
 from matplotlib import pyplot as plt
 import sqlite_lib
 
-timestamp, temp, hum, xTicks = [], [], [], []
 
-data = sqlite_lib.get_analytics_data()
+class Analytics:
+    @staticmethod
+    def main():
+        Analytics.gen_img_matplot()
+        Analytics.gen_img_lib2()
 
-# print(data)
+    @staticmethod
+    def gen_img_matplot():
+        data = sqlite_lib.get_analytics_data()
+        timestamp, temp_set, hum_set = [], [], []
+        x_ticks = []
 
-for row in data:
-    timestamp.append(row[0].split(" ")[1][:5])
-    temp.append(row[1])
-    hum.append(row[2])
+        """ Split the data and inert into appropriate set"""
+        for row in data:
+            timestamp.append(row[0].split(" ")[1][:5])
+            temp_set.append(row[1])
+            hum_set.append(row[2])
 
-for x in range(0, 25, 2):
-    xTicks.append("{:02}:00".format(x))
+        """ Generate hour stamp from 00:00 to 24:00"""
+        for hour_stamp in range(0, 25, 2):
+            x_ticks.append("{:02}:00".format(hour_stamp))
 
-fig = plt.figure(dpi=320, figsize=(10, 5))
-ax1 = fig.add_subplot(1, 1, 1)
-ax2 = ax1.twinx()
+        fig = plt.figure(dpi=320, figsize=(10, 5))
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax2 = ax1.twinx()
 
-ax1.plot(timestamp, temp, 'r-')
-ax2.plot(timestamp, hum, 'b-')
+        ax1.plot(timestamp, temp_set, 'r-')
+        ax2.plot(timestamp, hum_set, 'b-')
 
-ax1.set_xticks(xTicks)
-ax1.set_ylabel('Temperature(℃)', color='r')
-ax2.set_ylabel('Humidity(%)', color='b')
+        ax1.set_xticks(x_ticks)
+        ax1.set_xticklabels(x_ticks, rotation=0)
+        ax1.set_ylabel('Temperature(℃)', color='r')
+        ax2.set_ylabel('Humidity(%)', color='b')
 
-fig.autofmt_xdate()
-fig.savefig('matplot.png', dpi=fig.dpi)
-plt.show()
+        fig.savefig('matplot.png')
+        # plt.show()
+
+    @staticmethod
+    def gen_img_lib2():
+        # TODO use another library to generate the img
+        pass
+
+
+if __name__ == '__main__':
+    ANL = Analytics()
+    ANL.main()
