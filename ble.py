3c480b87f3ec5b9dd9e4632041f0c6aa54ed2e33
heiybb <hf.heiybb@gmail.com>
2019-04-03 21:58:13 +1100

    Fix the bug which will cause infinite loop and memory leak and redesign as OO

--- ble.py
+++ ble.py
@@ -3,15 +3,25 @@ import bluetooth
 from monitorAndNotify import MonitorData
 from push_service import PushThread
 
-SEND = False
-while not SEND:
-    print("Scanning...")
-    nearbyDevices = bluetooth.discover_devices()
 
-    for macAddress in nearbyDevices:
-        if '40:4E:36:A9:DF:4D' in macAddress:
-            print("Muggle's Pixel2 Found")
-            MON = MonitorData()
-            PushThread('BLE Notify', MON.to_string()).start()
-            SEND = True
-            break
+class BLENotify:
+    @staticmethod
+    def ble_search_notify():
+        attempt = 10
+        while attempt > 0:
+            print("Scanning...")
+            nearby_devices = bluetooth.discover_devices()
+
+            for mac_address in nearby_devices:
+                if '40:4E:36:A9:DF:4D' in mac_address:
+                    print("Muggle's Pixel2 Found")
+                    mon = MonitorData()
+                    PushThread('BLE Notify', mon.to_string()).start()
+                    return
+
+            attempt = attempt - 1
+            print("Remain attempts:", attempt)
+
+
+if __name__ == '__main__':
+    BLENotify.ble_search_notify()
