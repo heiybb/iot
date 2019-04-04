778168fe4d093831b2fc20a0b3ec1c6b8b33b61b
heiybb <hf.heiybb@gmail.com>
2019-04-04 16:29:56 +1100

    Pylint Optimise For sqlite lib

--- sqlite_lib.py
+++ sqlite_lib.py
@@ -1,35 +1,45 @@
+"""
+Wrap all the function the project need as a simple library
+"""
+
 import logging
 import os
 import sqlite3
 from sqlite3 import Error
 
 DB_FILE = './tmDB.db'
-INIT_TABLE = """CREATE TABLE IF NOT EXISTS TM_DATA (timestamp TEXT primary key, temperature REAL, humidity REAL)"""
+INIT_TABLE = """CREATE TABLE IF NOT EXISTS TM_DATA
+            (timestamp TEXT primary key, temperature REAL, humidity REAL)"""
 
 
 def initialize():
     """ Create the database file and initialize the table if not exist """
-    global conn
     if not os.path.isfile(DB_FILE):
+        connection = sqlite3.connect(DB_FILE)
         try:
-            conn = sqlite3.connect(DB_FILE)
-            conn.cursor().execute(INIT_TABLE)
-            conn.commit()
-        except Error as e:
-            print(e)
+            with connection:
+                connection.execute(INIT_TABLE)
+        except Error as error:
+            print(error)
         finally:
-            conn.close()
+            connection.close()
     else:
         pass
 
 
-# Insert data to the project database file
 def insert_data(timestamp, temperature, humidity):
+    """
+    Insert timestamp temperature humidity to database
+    :param timestamp: time in datetime format
+    :param temperature: float with precision 00.00
+    :param humidity: float with precision 00.00
+    :return: None
+    """
     connection = sqlite3.connect(DB_FILE)
     try:
         with connection:
             connection.execute(
-                """INSERT INTO TM_DATA (timestamp, temperature, humidity) 
+                """INSERT INTO TM_DATA (timestamp, temperature, humidity)
                 VALUES (?, ?, ?)""", (timestamp, temperature, humidity))
     except sqlite3.Error as error:
         logging.error("Database Error: %s", error)
@@ -38,60 +48,91 @@ def insert_data(timestamp, temperature, humidity):
 
 
 def query_all_th_data():
-    global conn
+    """
+    Query all data from the database file and print them out
+    :return: None
+    """
+    connection = sqlite3.connect(DB_FILE)
     try:
-        conn = sqlite3.connect(DB_FILE)
-        exe = conn.cursor()
-        c = exe.execute("""select * from TM_DATA""")
-        for row in c:
+        with connection:
+            result = connection.execute("""select * from TM_DATA""")
+        for row in result:
             print('TimeSlot:' + row[0])
             print('Temperature:' + str(row[1]))
             print('Humidity:' + str(row[2]))
 
-    except Error as e:
-        print(e)
+    except Error as error:
+        print(error)
     finally:
-        conn.close()
+        connection.close()
 
 
-# Query min max temperature and humidity and order by data
 def get_csv_data():
+    """
+    Query min max temperature and humidity and order by data
+    :return: None
+    """
     data = []
-    global conn
+    connection = sqlite3.connect(DB_FILE)
     try:
-        conn = sqlite3.connect(DB_FILE)
-        exe = conn.cursor()
-        c = exe.execute(
-            """SELECT strftime('%Y-%m-%d',timestamp),MIN(temperature),MAX(temperature),MIN(humidity),MAX(humidity)
-            FROM TM_DATA
-            GROUP BY DATE(timestamp)
-            ORDER BY DATE(timestamp)""")
+        with connection:
+            result = connection.execute(
+                """SELECT strftime('%Y-%m-%d',timestamp),
+                MIN(temperature),MAX(temperature),MIN(humidity),MAX(humidity)
+                FROM TM_DATA
+                GROUP BY DATE(timestamp)
+                ORDER BY DATE(timestamp)""")
 
-        for row in c:
+        for row in result:
             data.append(row)
-    except Error as e:
-        print(e)
+    except Error as error:
+        print(error)
     finally:
-        conn.close()
-        return data
+        connection.close()
+    return data
 
 
 def get_analytics_data():
+    """
+    Query the data used for the matplotlib library
+    :return: List format
+    """
     data = []
-    global conn
+    connection = sqlite3.connect(DB_FILE)
     try:
-        conn = sqlite3.connect(DB_FILE)
-        exe = conn.cursor()
-        c = exe.execute("""SELECT datetime(timestamp,'localtime') AS MEL,temperature,humidity FROM TM_DATA
-                        WHERE MEL LIKE '%2019-04-02%' AND strftime('%M', MEL)% 15 =0
-                        ORDER BY MEL""")
-        # c = exe.execute("""SELECT DISTINCT(strftime('%Y-%m-%d %H:%2',timestamp)),temperature,humidity
-        #                     FROM TM_DATA
-        #                     GROUP BY strftime('%Y-%m-%d %H',timestamp)""")
-        for row in c:
+        with connection:
+            result = connection.execute(
+                """SELECT DISTINCT(strftime('%Y-%m-%d %H:%M',datetime(timestamp,'localtime')))
+                as MEL,temperature,humidity FROM TM_DATA
+                WHERE (MEL LIKE '%2019-04-02%')
+                AND strftime('%M', MEL)% 15 = 0""")
+        for row in result:
             data.append(row)
-    except Error as e:
-        print(e)
+    except Error as error:
+        print(error)
     finally:
-        conn.close()
-        return data
+        connection.close()
+    return data
+
+
+def get_analytics_echart_data():
+    """
+    Query data used for the pyechart library
+    :return: List format
+    """
+    data = []
+    connection = sqlite3.connect(DB_FILE)
+    try:
+        with connection:
+            result = connection.execute(
+                """SELECT  DISTINCT(strftime('%Y-%m-%d %H:%M',datetime(timestamp,'localtime')))
+                as MEL,temperature,humidity FROM TM_DATA
+                WHERE (MEL LIKE '%2019-04-02%' OR MEL LIKE '%2019-04-03%')
+                AND strftime('%M', MEL)% 15 = 0""")
+        for row in result:
+            data.append(row)
+    except Error as error:
+        print(error)
+    finally:
+        connection.close()
+    return data
